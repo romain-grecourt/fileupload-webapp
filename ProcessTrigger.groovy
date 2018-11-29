b197f154e5b832e6f87f0ceea2095f81a625662e
Romain Grecourt <romain.grecourt@oracle.com>
2018-11-28 17:35:07 -0800

    Update ProcessTrigger.groovy

--- ProcessTrigger.groovy
+++ ProcessTrigger.groovy
@@ -1,27 +1,29 @@
 node {
-   stage('process-trigger') {
-     def causes = currentBuild.rawBuild.getCauses()
-     if(causes.size() > 0
-        && causes[0].getClass().getName() == "org.jenkinsci.plugins.ghprb.GhprbCause"){
-        def commentBody = currentBuild.rawBuild.getCauses()[0].getCommentBody()
-
-        // this is illustrative.
-        // the ghprb plugin can be configured with a loose regexp
-        // trigger different job based on the comment content
-        if(commentBody.contains("tests")){
-            trigger_pipeline("fileupload-webapp", env.ghprbPullId)
+    stage('process-trigger') {
+        def commentBody = get_comment_trigger();
+        if(commentBody != null){
+            // this is illustrative.
+            // the ghprb plugin can be configured with a loose regexp
+            // trigger different job based on the comment content
+            if(commentBody.contains("tests")){
+                build job: "fileupload-webapp/PR-${ghprbPullId}"
+            } else {
+                // fail here if the comment content is supported
+                error("Unsupported comment message")
+            }
         } else {
-            // fail here if the comment content is supported
-            error("Unsupported comment message")
+            // only process comment based trigger
+            error("Unsupported trigger")
         }
-     } else {
-        // only process comment based trigger
-        error("Unsupported trigger")
-     }
-   }
+    }
 }
 
 @NonCPS
-def trigger_pipeline(name, prId) {
-    build job: "${name}/PR-${prId}"
+def get_comment_trigger() {
+    def causes = currentBuild.rawBuild.getCauses()
+    if(causes.size() > 0
+        && causes[0].getClass().getName() == "org.jenkinsci.plugins.ghprb.GhprbCause"){
+        return currentBuild.rawBuild.getCauses()[0].getCommentBody()
+    }
+    return null;
 }
